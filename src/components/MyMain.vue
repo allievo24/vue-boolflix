53279bc2f32c3584203757989cef100941dc4b87
saidmoussadek <saiddus24@gmail.com>
2022-08-31 18:10:00 +0200

    chiamata axios riuscita 18:10

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -2,8 +2,9 @@
    <div>
      <ul>
         <li> Titolo- titolo originale- voto- lingua</li>
-        <li v-for="(film, index) in listaFilm" :key="index"></li>
+        <li v-for="(film, index) in listaFilm" :key="index">
         {{film.title}} - {{film.originale_title}}-{{film.vote_average}}-{{film.originale_language}}
+     </li>
      </ul>
    </div>
   
