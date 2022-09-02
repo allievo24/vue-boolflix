6e1cd43b81cccd09b2c75ef77fd972e9154ee974
saidmoussadek <saiddus24@gmail.com>
2022-09-02 13:07:23 +0200

    push inzio style

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,73 @@
 <template>
   <div id="app">
-   <MyHeader @search="ricerca"/>
-   <MyMain :listaFilm ="listaFilm"/>
-   
+    <MyHeader @search="ricerca" />
+    <MyMain :listaFilm="listaFilm" :listaSerie="listaSerie" />
   </div>
 </template>
 
 <script>
-import MyHeader from './components/MyHeader.vue';
-import MyMain from './components/MyMain.vue';
-import axios from'axios';
-
+import MyHeader from "./components/MyHeader.vue";
+import MyMain from "./components/MyMain.vue";
+import axios from "axios";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     MyHeader,
-    MyMain
-  },
-  
-  data(){
-    return{
-      apilink :'https://api.themoviedb.org/3',
-      privateKey:'ed5299798d3d71d806d1feda5d373225',
-      lingua : 'it-IT',
-      listaFilm : []
-    
-    }
+    MyMain,
   },
 
-  
-  
-  
-  methods:{
-    ricerca(cerca){
-      axios.get(this.apilink +'/search/movie?api_key='+ this.privateKey + '&language=' + this.lingua + '&query=' + cerca )
-       .then(rest =>{
-        this.listaFilm = rest.data.results;
-
-       })
-  
-       .catch(error =>{
-        console.log(error);
-       })
-       
-       
-       
-     
-    }
+  data() {
+    return {
+      apilink: "https://api.themoviedb.org/3",
+      privateKey: "ed5299798d3d71d806d1feda5d373225",
+      lingua: "it-IT",
+      listaFilm: [],
+      listaSerie: [],
+    };
   },
 
+  methods: {
+    ricerca(cerca) {
+      axios
+        .get(
+          this.apilink +
+            "/search/movie?api_key=" +
+            this.privateKey +
+            "&language=" +
+            this.lingua +
+            "&query=" +
+            cerca
+        )
+        .then((rest) => {
+          this.listaFilm = rest.data.results;
+        })
 
+        .catch((error) => {
+          console.log(error);
+        });
 
+      axios
+        .get(
+          this.apilink +
+            "/search/tv?api_key=" +
+            this.privateKey +
+            "&language=" +
+            this.lingua +
+            "&query=" +
+            cerca
+        )
+        .then((rest) => {
+          this.listaSerie = rest.data.results;
+        })
 
-
-
-}
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
 </style>
--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -1,19 +1,47 @@
 <template>
   <div>
-    <ul>
-      <li>Titolo- titolo originale- voto- lingua</li>
-      <li v-for="(film, index) in listaFilm" :key="index">
-        {{ film.title }}------------
-        {{film.original_title}}
-        <!--integro bandiere con un v-bind -->
-        <img v-if="bandiere.includes(film.original_language)" :src=" '../assets/img/ '+film.original_language+'.jpg'" alt="poster film"/>
-        {{ film.original_language }}----
-        {{film.vote_average}}-----
-        {{ film.overview }}
-       
-          
-      </li>
-    </ul>
+    <section class="film">
+      <h2>Lista Film</h2>
+         
+         <div class="flip-card" v-for="(film, index) in listaFilm" :key="index">
+            <div class="flip-card-inner">
+               <div class="flip-card-front" >
+                <img :src="'https://image.tmdb.org/t/p/w200/'+ film.poster_path" alt=""/>
+               </div>
+               <div class="flip-card-back"></div>
+                
+               {{ film.title }}------------
+               {{film.original_title}}
+               <!--integro bandiere con un v-bind -->
+               <img class="flag" v-if="bandiere.includes(film.original_language)" 
+               :src=" require('../assets/img/' + film.original_language + '.jpg')" />
+               {{ film.original_language }}----
+               {{film.vote_average}}-----
+               {{ film.overview }}
+            </div>
+
+         </div>
+    </section>
+    <section class="serie">
+      <h2>Lista Serie</h2>
+         <ul>
+               <li>Titolo- titolo originale- voto- lingua</li>
+               <li v-for="(film, index) in listaSerie" :key="index">
+               {{ film.name }}------------
+               {{film.original_name}}
+                <img :src="'https://image.tmdb.org/t/p/w200/'+ film.poster_path" alt=""/>
+
+               <!--integro bandiere con un v-bind -->
+               <img class="flag" v-if="bandiere.includes(film.original_language)" 
+               :src=" require('../assets/img/' + film.original_language + '.jpg')" />
+               {{ film.original_language }}----
+               {{film.vote_average}}-----
+               {{ film.overview }}
+
+               </li>
+            </ul>
+    </section>
+    
   </div>
 </template>
 
@@ -22,6 +50,8 @@ export default {
   nome: "MyMain",
   props: {
     listaFilm: Array,
+    listaSerie: Array,
+
   },
   data() {
     return {
@@ -37,7 +67,57 @@ export default {
 </script>
 
 <style lang="scss">
-li {
-  margin: 2rem 0;
-}
+   .film{
+      display: flex;
+         
+         .flip-card {
+         background-color: transparent;
+         width: 300px;
+         height: 300px;
+         perspective: 1000px;
+         }
+
+         .flip-card-inner {
+         position: relative;
+         width: 100%;
+         height: 100%;
+         text-align: center;
+         transition: transform 0.6s;
+         transform-style: preserve-3d;
+         box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+         }
+
+         .flip-card:hover .flip-card-inner {
+         transform: rotateY(180deg);
+         }
+
+         .flip-card-front, .flip-card-back {
+         position: absolute;
+         width: 100%;
+         height: 100%;
+         -webkit-backface-visibility: hidden;
+         backface-visibility: hidden;
+         }
+
+         .flip-card-front {
+         background-color: #bbb;
+         color: black;
+
+         }
+
+         .flip-card-back {
+        // background-color: #2980b9;
+         color: white;
+         transform: rotateY(180deg);
+         }
+            
+         }
+         .flag{            
+            width: 50px;
+         }
+   
+   
+
+
+
 </style>
